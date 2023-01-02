8361143bc90d9f5411a6046fa0a0b567fc51c8b5
진희 <ha9ha6@naver.com>
2023-01-03 01:05:51 +0900

    소켓 통신 연습_책보고 함

--- 새 파일.py
+++ 새 파일.py
@@ -1,8 +1,32 @@
-아 됐나요? 
-리얼로?
-print("Hello world") 
-요기 지움ㅎ
-왕ㅎㅎㅎㅎ
-벌써 12월이 됨
-커밋을 하는 중
-ㅎ
+from socket import *
+import os
+
+
+def parsing(host): # raw socket 생성 및 bind
+    if os.name=="nt":
+        sock_protocol=IPPROTO_IP
+    else:0
+        sock_protocol=IPPROTO_ICMP
+    sock=socket(AF_INET, SOCK_RAW, sock_protocol)
+    sock.bind((host, 0))
+    
+    sock.setsockopt(IPPROTO_IP, IP_HDRINCL, 1) #socket옵션
+    
+    if os.name=="nt":
+        sock.ioctl(SIO_RCVALL. RCVALL_ON) #promiscuous mode
+    
+    data=sock.recvfrom(65535)
+    print(data[0])
+    
+    if os.name=="nt":
+        sock.ioctl(SIO_RCVALL, RCCALL_OFF) #promiscuous mode 끄기
+        
+        sock.close() #소켓 종료
+        
+        if __name__=="__main__":
+            host="192.168.45.184" # 자신의 IP주소로 변경
+            print(f"Listening at [{host}]")
+            parsing(host)
+        
+    
+    
